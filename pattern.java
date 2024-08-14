4221a1246335bffedf63bff0733785b4e10f896d
ayushjaiswal21 <161676150+ayushjaiswal21@users.noreply.github.com>
2024-08-14 16:46:22 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,35 @@
+//patern1
+import java.util*;
+public class pattern{
+  public static void main(String args[]){
+    int n=4;
+    int m=5;
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=m:;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
+
+//patern2
+public class pattern{
+  public static void main(String args[]){
+    int n=4;
+    int m=5;
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=m:;j++){
+        if(i==a||i==1){
+          System.out.print("*");
+        }else if (j==b||j==1){
+          System.out.print("*");
+        }else{
+          System.out.print(" ");
+        }
+      }
+    System.out.println();
+    }
+  }
+}
+  
